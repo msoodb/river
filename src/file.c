504ac74e378cb50e90ff19fa12646bf588334f3c
masoud bolhassani <masoud.bolhassani@gmail.com>
2019-09-12 02:47:11 +0430

    file

--- src/file.c
+++ src/file.c
@@ -1,4 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(){
+  FILE *fptr = NULL;
+  fptr = fopen("numbers.text", "w");
+  if(fptr == NULL){
+    printf("ERROR!");
+    exit(1);
+  }
+  fprintf(fptr, "hello file! number: %d\n", 42);
+  fclose(fptr);
   return 0;
 }
