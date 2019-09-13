0535dcdc6d0f1a688b62a07e3e03090848907fe5
masoud bolhassani <masoud.bolhassani@gmail.com>
2019-09-13 04:33:40 +0430

    read file

--- src/file.c
+++ src/file.c
@@ -8,7 +8,22 @@ int main(){
     printf("ERROR!");
     exit(1);
   }
-  fprintf(fptr, "hello file! number: %d\n", 42);
+  fprintf(fptr, "hello\n");
   fclose(fptr);
+
+  //------------------------------------------
+
+  FILE *fptrRead = NULL;
+  fptrRead = fopen("numbers.text", "r");
+  if(fptrRead==NULL){
+    printf("error! file does not exist.\n");
+    exit(1);
+  }
+  char c;
+  //while((c=fgetc(fptrRead)) != EOF){
+  //printf("%c\n", c);  
+  //}
+  fscanf(fptrRead, "%c", &c);
+  printf("%c\n", c);
   return 0;
 }
