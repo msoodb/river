45e48b1c4c3eca45ef6e2fb9d1109b84b1e7ecbb
masoud bolhassani <masoud.bolhassani@gmail.com>
2019-09-13 22:16:03 +0430

    init.el

---
+++ init.el
@@ -0,0 +1,30 @@
+(setq inhibit-startup-message t)
+
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/"))
+
+(package-initialize)
+
+;; Bootstrap `use-package'
+(unless (package-installed-p 'use-package)
+OB  (package-refresh-contents)
+  (package-install 'use-package))
+
+(use-package try
+  :ensure t)
+
+(use-package auto-complete
+  :ensure t
+  :init
+  (progn
+    (ac-config-default)
+    (global-auto-complete-mode t)
+    ))
+
+(use-package yasnippet
+  :bind
+  :config
+  (yas-global-mode 1))
+
