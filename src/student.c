bbbeedd5fe1e515f484e941b0117763e068792ad
masoud bolhassani <masoud.bolhassani@gmail.com>
2019-09-14 20:44:30 +0430

    student

--- src/student.c
+++ src/student.c
@@ -4,7 +4,7 @@ int main(int argc, char *argv[])
 {
   FILE *fp = fopen("student.csv", "r");
   if (fp == NULL){
-    printf("%s\n", "File does not exist.");
+    printf("%s\n", "File does not exist");
     exit(1);
   }
   return 0;
