78520328c09b387d0847123f75b6106cbc885b6c
masoud bolhassani <masoud.bolhassani@gmail.com>
2019-09-21 02:02:55 +0300

    pattern

--- src/pattern.c
+++ src/pattern.c
@@ -4,9 +4,25 @@ int main(int argc, char *argv[])
 {
 	int n;
 	n = 13;
-	for (int i = 0; i < n; ++i) {
+	for (int i=0; i<n; ++i) {
 		int k=0;
-		while (k < n) {
+		for (int j = n; j>i ; --j) {
+			printf(" ");
+		}
+
+		while (k <= i) {
+			printf("* ");
+			k++;
+		}
+		printf("\n");
+	}
+	for (int i=n; i>0; --i) {
+		int k=0;
+		for (int j = n; j>i ; --j) {
+			printf(" ");
+		}
+
+		while (k <= i) {
 			printf("* ");
 			k++;
 		}
