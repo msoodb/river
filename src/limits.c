ef1bad13ca3ddec71dceff9c4609eb65888e05bb
masoud bolhassani <masoud.bolhassani@gmail.com>
2019-09-22 00:32:18 +0300

    limits

---
+++ src/limits.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main(int argc, char *argv[])
+{
+	printf("there are %d bit in One Byte\n", CHAR_BIT);
+	printf("int minimum is : %d\n", INT_MIN);
+	return 0;
+}
