9c030305639458588a84000f2df117f6d8245261
masoud bolhassani <masoud.bolhassani@gmail.com>
2019-09-23 02:24:15 +0300

    linkedlist

--- src/linked_list.c
+++ src/linked_list.c
@@ -1,73 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct data
-{
-	int key;
-	int value;
-};
-	
-
 struct node
 {
-	struct data *n_data;
+	int data;
 	struct node *next;
 };
 
-struct node* push(struct node *head, int key, int value)
-{
-	struct node *node_c = (struct node *)malloc(sizeof(struct node));
-	if (node_c == NULL) {
+void push(struct node *head, int data)
+{	
+	/* create tmp node */
+	struct node *tmp = (struct node *)malloc(sizeof(struct node));
+	if (tmp == NULL) {
 		printf("%s\n", "out of memory!");
-		return NULL;
+		return;
 	}
-	struct data *data_c = (struct data*)malloc(sizeof(struct data));
-	if (data_c == NULL) {
-		printf("%s\n", "out of memory!");
-		return NULL;
-	}
-	data_c->key = key;
-	data_c->value = value;
-	node_c->n_data = data_c;
-	node_c->next = head;
+	tmp->data = data;
+	tmp->next = NULL;
 
-	return node_c;
+	/* insert tmp node in list */
+	tmp->next = head->next;
+	head->next = tmp;
+
+	/* return tmp node */
+	return;
 };
 
 int print(struct node* head)
 {
+	if(head == NULL){
+		return 0;
+	}
+	struct node* tmp = head->next;
 	do
 	{
-		printf("key : %d, value : %d\n", head->n_data->key, head->n_data->value);
-		head = head->next;
-	} while (head->next != NULL);
-	return 1;
+		printf("%d\n", tmp->data);
+		tmp = tmp->next;
+		
+	} while (tmp != NULL);	
 }
 
 int main(int argc, char *argv[])
 {
-	struct node *head;
-	head = (struct node *)malloc(sizeof(struct node));
-	if (head == NULL) {
+	struct node *numbers = NULL;
+	numbers  = (struct node *)malloc(sizeof(struct node));
+	if (numbers == NULL) {
 		printf("%s\n", "out of memory!");
-		exit(1);
+		return 1;
 	}
-	struct data *d = (struct data*)malloc(sizeof(struct data));
-	if (d == NULL) {
-		printf("%s\n", "out of memory!");
-		exit(1);
-	}
-	d->key = 0;
-	d->value = 42;
-	head->n_data = d;
-
-	struct node *second = push(head, 1, 43);
-	struct node *current = push(second, 2, 44);
-	
-	if (current == NULL) {
-		printf("%s\n", "out of memory!");
-		exit(1);	
-	}
-	print(current);
+	push(numbers, 14);
+	push(numbers, 98);
+	push(numbers, 42);
+	print(numbers);
 	return 0;
 }
