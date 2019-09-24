762d1f38e9eafd26de52d6aafa8d4d02894a1ae2
masoud bolhassani <masoud.bolhassani@gmail.com>
2019-09-24 14:49:56 +0300

    link

--- src/linked_list.c
+++ src/linked_list.c
@@ -1,86 +1,94 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct node
+struct data
 {
-	int data;
-	struct node *next;
+	int id;
+	char *name;
+	char *lastname;
+	char *addres;
+	int status;
 };
 
-void push(struct node** ptrhead, int data)
-{		
-	struct node* tmp = (struct node*)malloc(sizeof(struct node));
-	if (tmp == NULL) {
-		printf("%s\n", "out of memory!");
-		return;
-	}
-
-	tmp->data = data;
-	tmp->next = *ptrhead;
-	*ptrhead = tmp;
-}
-
-void insert(struct node** ptrhead, int position, int data)
+struct data* createStudent(int id, char *name, char *lastname, char *addres, int status)
 {
-	struct node* tmp = (struct node*)malloc(sizeof(struct node));
-	if (tmp == NULL) {
-		printf("%s\n", "out of memory!");
-		return;
-	}
-	tmp->data = data;
-	tmp->next = NULL;
-	
-	if (position == 1) {
-		tmp->next = *ptrhead;
-		*ptrhead = tmp;
-		return;
-	}
-	struct node *tmp2 = *ptrhead;
-	for (int i = 0; i < position-2; ++i) {
-		tmp2 = tmp2->next;			
-	}
-	tmp->next = tmp2->next;
-	tmp2->next = tmp;
+	struct data *tmp = (struct data*) malloc(sizeof(struct data));
+	if(tmp == NULL){
+		printf("out of memory\n");
+		return NULL;
+	}		
+	tmp->id = id;
+	tmp->name = name;
+	tmp->lastname = lastname;
+	tmp->addres = addres;
+	tmp->status = status;
+
+	return tmp;
 }
 
-int pop(struct node** ptrhead, int *data)
+void printStudentHeader()
 {
-	if(ptrhead == NULL)
-		return 0;
-	*data = (*ptrhead)->data;
-	*ptrhead = (*ptrhead)->next;
-	
-}
-
-void print(struct node* head)
-{		
-	do
-	{
-		printf("%d ", head->data);
-		head = head->next;
-		
-	} while (head != NULL);
+	printf("%-6s", "id");
+	printf("%-20s", "name");
+	printf("%-20s", "lastname");
+	printf("%-30s", "addres");
+	printf("%-6s", "status");
+	printf("\n");
+	printf("---------------------------------------------------------------------------------------");
 	printf("\n");
 }
 
+void printStudent(struct data *student)
+{
+	printf("%-6d", student->id);
+	printf("%-20s", student->name);
+	printf("%-20s", student->lastname);
+	printf("%-30s", student->addres);
+	printf("%-6d", student->status);
+	printf("\n");
+}
 
+struct node
+{
+	struct data *data_c;
+	struct node *next;
+	
+};
+
+void push(struct node **ptrhead, struct data *data)
+{
+	struct node *tmp = (struct node *)malloc(sizeof(struct node));
+	if (tmp == NULL) {
+		printf("out of memory!\n");
+		return;
+	}
+	tmp->data_c = data;
+	tmp->next = NULL;
+
+	if (*ptrhead != NULL) {
+		tmp->next = (*ptrhead)->next;		
+	}
+	*ptrhead = tmp;
+
+}
+void print(struct node *ptrhead)
+{
+	printStudentHeader();
+		
+	while (ptrhead != NULL) {
+		printStudent(ptrhead->data_c);
+		ptrhead = ptrhead->next;
+	}
+}
 
 int main(int argc, char *argv[])
 {
-	struct node* numbers = NULL;
-	
-	push(&numbers, 14);
-	push(&numbers, 98);
-	push(&numbers, 42);
-	print(numbers);
-	insert(&numbers,3, 99);
-	print(numbers);
+	struct node *students = NULL;
 
-	int a;
-	pop(&numbers, &a);
-	printf("pop: %d\n", a);	
-	print(numbers);
-	
-	free(numbers);
+	/* create data */
+	struct data *student;
+	student = createStudent(101, "masoud", "bolhassani", "Tallinn, Estonia", 1);
+	push(&students, student);
+	print(students);
 	return 0;
 }
