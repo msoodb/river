d9cbf6c969299d01ddc2da9fca03a3dcc23071de
masoud bolhassani <masoud.bolhassani@gmail.com>
2019-09-29 22:47:02 +0300

    insert

--- src/binary.c
+++ src/binary.c
@@ -12,7 +12,7 @@ struct node
 {
 	struct element *data;
 	struct node *left;
-	struct node *right;	
+	struct node *right;
 };
 
 int elmcmp(struct element *elm1, struct element *elm2)
@@ -20,28 +20,83 @@ int elmcmp(struct element *elm1, struct element *elm2)
 	return strcmp(elm1->name, elm2->name);
 }
 
+struct node *create_node(struct element *e)
+{
+	struct node *new = (struct node *)malloc(sizeof(struct node));
+	new->data = e;
+	new->left = NULL;
+	new->right = NULL;
+}
+
 void insert(struct node **root, struct element *e)
 {
-	if (*root == NULL) {
-		*root = (struct node *)malloc(sizeof(struct node));
-		(*root)->data = e;
+	struct node *new = create_node(e);
+	struct node *current = *root;
+	struct node *itr;
+		
+	itr = current;
+	while (itr != NULL) {
+		current = itr;
+		if (e->id > itr->data->id) {			
+			itr = itr->right;
+		}else if (e->id < itr->data->id){			
+			itr = itr->left;
+		}else{
+			return;
+		}
 	}
+
+	if (current == NULL) {
+		*root = new;		
+		return;
+	}
+
+	if (e->id > current->data->id) {
+		current->right = new;
+	}else if (e->id < current->data->id){
+		current->left = new;
+	}else{
+		return;
+	}
+	return;
 }
 
 void print(struct node *root)
 {
-	printf("%s\n", root->data->name);
+	if (root == NULL) {
+		return;
+	}
+	printf("%d\n", root->data->id);
+	if (root->left != NULL) {
+		printf("%d  ", root->left->data->id);
+	}else {
+		printf("---");
+	}
+	if (root->right != NULL) {
+		printf("%d  ", root->right->data->id);
+	}else {
+		printf("---");
+	}
+	printf("\n");
+	print(root->left);
+	print(root->right);
 }
 
 int main(int argc, char *argv[])
 {
 	struct node *root = NULL;
 	struct element *e = NULL;
-	e = (struct element *)malloc(sizeof(struct element));
-	e->id = 1;
-	e->name = "root node";
-	insert(&root, e);
-	print(root);
 	
+	int i;
+	int N = 10;
+	int numbers[10] = {10, 6, 3, 7, 12, 11 ,34, 4, 9, 23}; 
+	//srand(time(0));
+	for (i = 0; i < N; ++i) {
+		e = (struct element *)malloc(sizeof(struct element));
+		e->id = numbers[i];
+		e->name = "root node";
+		insert(&root, e);		
+	}
+	print(root);	
 	return 0;
 }
