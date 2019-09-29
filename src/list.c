3efe68a2d67450b968f08d71efdd37e846ac7d79
masoud bolhassani <masoud.bolhassani@gmail.com>
2019-09-30 02:39:01 +0300

    delete

--- src/list.c
+++ src/list.c
@@ -23,6 +23,7 @@ struct node *create_node(int data)
 		goto out;
 	}	
 	new_node->data = data;
+	new_node->next = NULL;
 out:
 	return new_node;
 }
@@ -35,6 +36,40 @@ void push(struct node **ptr_head, int data)
 	current = *ptr_head;
 	*ptr_head = new_node;
 	new_node->next = current;
+}
+
+void append(struct node **ptr_head, int data)
+{
+	if (*ptr_head == NULL) {
+		push(ptr_head, data);
+		goto out;
+	}
+	
+	struct node *current;
+	struct node *itr;
+
+	current = *ptr_head;
+	itr = current;
+
+	while (itr != NULL) {
+		current = itr;
+		itr = itr->next;
+	}
+	push(&current->next, data);
+out:
+	return;
+}
+
+void delete (struct node **ptr_head)
+{
+	struct node *current;
+	current = *ptr_head;
+	
+	while (*ptr_head) {
+		current = *ptr_head;
+		*ptr_head = (*ptr_head)->next;
+		free(current);
+	}
 }
 
 void print(struct node *head)
@@ -50,6 +85,8 @@ int main(int argc, char *argv[])
 	struct node *head;
 	push(&head, 4);
 	push(&head, 6);
+	append(&head, 7);
 	print(head);
+	delete(&head);
 	return 0;
 }
